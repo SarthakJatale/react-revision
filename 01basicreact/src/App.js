8bcba24d9155bcebad955233f7df7c77fa6151f0
SarthakJatale <sarthakjatale4@gmail.com>
2024-01-08 17:12:02 +0530

    My first React App

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Sarthak Jatale</h1>
+  );
+}
+
+export default App;
---
+++ vite-project/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+    <h1>Hello SARTHAK | avaya</h1>
+  )
+}
+
+export default App
