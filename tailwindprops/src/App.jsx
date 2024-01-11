af7e771d5fd92e0dd95e649de8572fa49c7b2ecd
SarthakJatale <sarthakjatale4@gmail.com>
2024-01-11 13:19:37 +0530

    react router crash course

---
+++ tailwindprops/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      return (
+  <>
+    <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1>
+    <div>
+
+    </div>
+    
+  </>
+)
+    </>
+  )
+}
+
+export default App
